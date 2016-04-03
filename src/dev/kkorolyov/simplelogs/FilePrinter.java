47709d1c0c5bd64e57b2d09288a3033f9bf785af
kkorolyov <kirillk753@gmail.com>
2016-04-02 17:38:19 -0700

    Update FilePrinter javadoc

--- src/dev/kkorolyov/simplelogs/FilePrinter.java
+++ src/dev/kkorolyov/simplelogs/FilePrinter.java
@@ -8,12 +8,13 @@ import java.io.IOException;
 /**
  * Prints a message to a file.
  */
-public class FilePrinter implements Printer {
+public class FilePrinter implements Printer {	// TODO Avoid overwriting existing files
 	private File file;
 	private BufferedWriter writer;
 	
 	/**
 	 * Creates a new file printer printing to a file of the specified name.
+	 * If a file of the specified name already exists, all of its data will be cleared!
 	 * @param filename name of file to print to
 	 */
 	public FilePrinter(String filename) {
@@ -21,6 +22,7 @@ public class FilePrinter implements Printer {
 	}
 	/**
 	 * Creates a new file printer printing to the specified file.
+	 * If a file of the specified name already exists, all of its data will be cleared!
 	 * @param file file to print to
 	 */
 	public FilePrinter(File file) {
