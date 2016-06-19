32141b8ef2066a5aa08a0fce6502002f23768be7
kkorolyov <kirillk753@gmail.com>
2016-06-19 13:21:15 -0700

    Overhaul Properties

--- src/dev/kkorolyov/simpleprops/Properties.java
+++ src/dev/kkorolyov/simpleprops/Properties.java
@@ -1,206 +1,234 @@
 package dev.kkorolyov.simpleprops;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Provides access to and mutation of long-term properties, which are an indefinite set of key-value pairs.
- * Provides for saving and loading data to/from a file unique to the current instance.
+ * Provides for saving and loading data to/from a file.
  */
 public class Properties {
-	private static final Map<String, Properties> instances = new HashMap<>();
+	private static final String DELIMETER = "=",
+															COMMENT = "#";
 	
-	private final String filename;
-	private final Map<String, String> defaultProperties = new HashMap<>(),
-																		properties = new HashMap<>();
+	private final List<String>	keys = new ArrayList<>(),
+															values = new ArrayList<>();
+	private final Map<String, Integer> keyPositions = new HashMap<>();
+	private File file;
+	private Properties defaults;
 	
 	/**
-	 * Returns a {@code Properties} instance of the specified name.
-	 * If an appropriate instance does not yet exist, it is created using the specified filename and no default properties.
-	 * @see #getInstance(String, Map)
+	 * Constructs a new {@code Properties} instance with no backing file.
 	 */
-	public static Properties getInstance(String filename) {
-		return getInstance(filename, (Map<String, String>) null);
+	public Properties() {
+		this(null);
 	}
 	/**
-	 * Functions similarly to {@link #getInstance(String, Map)}, but uses a 2-dimensional array instead of a {@code Map} to specify default properties.
+	 * Constructs a new {@code Properties} instance for a specified file.
+	 * @see #Properties(File, Properties, boolean)
 	 */
-	public static Properties getInstance(String filename, String[][] defaultProperties) {
-		return getInstance(filename, convertArrayToMap(defaultProperties));
+	public Properties(File file) {
+		this(file, null);
 	}
 	/**
-	 * Returns a {@code Properties} instance for the specified filename.
-	 * If an appropriate instance does not yet exist, it is created using the specified filename and default properties.
-	 * @param filename label for the instance, as well as the name of the file written to when saved
-	 * @param defaultProperties default properties of new instance; ignored if retrieving an existing instance
-	 * @return appropriate instance
+	 * Constructs a new {@code Properties} instance for a specified file and with specified default values.
+	 * @see #Properties(File, Properties, boolean)
 	 */
-	public static Properties getInstance(String filename, Map<String, String> defaultProperties) {
-		Properties instance = null;
-		
-		while ((instance = instances.get(filename)) == null)
-			instances.put(filename, new Properties(filename, defaultProperties));
-		
-		return instance;
+	public Properties(File file, Properties defaults) {
+		this(file, defaults, false);
 	}
-	
-	private static Map<String, String> convertArrayToMap(String[][] array) {
-		if (array == null)
-			return null;
-		
-		Map<String, String> convertedMap = new HashMap<>();
-		
-		for (String[] property : array) {
-			if (array.length >= 2)
-				convertedMap.put(property[0], property[1]);
-		}
-		return convertedMap;
-	}
-	
-	private Properties(String filename, Map<String, String> defaultProperties) {
-		this.filename = filename;
-		
-		setDefaultProperties(defaultProperties);
+	/**
+	 * Constructs a new {@code Properties} instance for a specified file and with specified default values. This method may optionally create the path to the specified file.
+	 * @param file backing filesystem file
+	 * @param defaults default properties
+	 * @param mkdirs if {@code true}, the path to the specified file is created if it does not exist
+	 */
+	public Properties(File file, Properties defaults, boolean mkdirs) {
+		setFile(file, mkdirs);
+		setDefaults(defaults);
 		loadDefaults();
 		try {
-			loadFromFile();
+			loadFile();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private void setDefaultProperties(Map<String, String> newDefaultProperties) {
-		defaultProperties.clear();
-		
-		if (newDefaultProperties != null) {	// If null, no default properties
-			for (String key : newDefaultProperties.keySet()) {
-				defaultProperties.put(key, newDefaultProperties.get(key));
-			}
-		}
-	}
-	
 	/**
-	 * Resets all properties to default values.
-	 */
-	public void loadDefaults() {
-		properties.clear();
-		
-		for (String key : defaultProperties.keySet()) {
-			properties.put(key, defaultProperties.get(key));
-		}
-	}
-	
-	/**
-	 * Retrieves the value of a property of the specified key.
-	 * If this method is called on an instance which has no properties in memory, the instance will first attempt to load both its default properties and properties from its respective file.
+	 * Retrieves the value of the property matching the specified key.
 	 * @param key key of property to retrieve
-	 * @return property value
+	 * @return property value, or {@code null} if no such property
 	 */
-	public String getValue(String key) {
-		reloadIfEmpty();
-		
-		return properties.get(key);
+	public String get(String key) {
+		return contains(key) ? values.get(keyPositions.get(key)) : null;
 	}
 	
 	/**
-	 * Returns the keys of all properties.
-	 * If this method is called on an instance which has no properties in memory, the instance will first attempt to load both its default properties and properties from its respective file.
- 	 * @return key of every property
- 	 */
-	public Set<String> getAllKeys() {
-		reloadIfEmpty();
-		
-		return properties.keySet();
-	}
-	
-	/** 
-	 * Returns the total number of properties of this instance.
-	 * If this method is called on an instance which has no properties in memory, the instance will first attempt to load both its default properties and properties from its respective file.
-	 * @return number of properties
-	 */
-	public int size() {
-		return getAllKeys().size();
-	}
-	
-	/**
-	 * Adds the specified property.
-	 * If the key matches an existing property's key, then that preexisting property's value is overridden by the specified value instead.
-	 * If this method is called on an instance which has no properties in memory, the instance will first attempt to load both its default properties and properties from its respective file.
+	 * Adds a property matching the specified key-value pair.
+	 * If the specified key matches an existing property's key, the preexisting key's value is overridden by the specified value.
 	 * @param key key of property to add
 	 * @param value value of property to add
 	 */
-	public void addProperty(String key, String value) {
-		reloadIfEmpty();
-
-		properties.put(key, value);
+	public void put(String key, String value) {
+		if (keyPositions.containsKey(key))
+			setKey(key, value);
+		else
+			addNewKey(key, value);
+	}
+	private void setKey(String key, String value) {
+		values.set(keyPositions.get(key), value);	// Change value at correct index
+	}
+	private void addNewKey(String key, String value) {
+		keys.add(key);
+		values.add(value);
+		
+		if (key != null)
+			keyPositions.put(key, keys.size() - 1);	// New key is at last index
 	}
 	
-	private void reloadIfEmpty() {
-		if (properties.isEmpty()) {	// Properties were possibly cleared to free memory
-			try {
-				loadDefaults();
-				loadFromFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+	/**
+	 * Removes the property matching the specified key.
+	 * @param key key of property to remove
+	 * @return removed property's value, or {@code null} if no such property
+	 */
+	public String remove(String key) {
+		String removedValue = null;
+		
+		if (keyPositions.containsKey(key)) {
+			int propertyIndex = keyPositions.remove(key);
+			
+			keys.remove(propertyIndex);
+			removedValue = values.remove(propertyIndex);
 		}
+		
+		return removedValue;
+	}
+	
+	/** @return {@code true} if this object contains a property matching the specified key */
+	public boolean contains(String key) {
+		return keyPositions.containsKey(key);
+	}
+	
+	/** @return list of all property keys, in the order they appear in this object */
+	public List<String> keys() {
+		List<String> toReturn = new LinkedList<>();
+		
+		for (String key : keys) {
+			if (key != null)
+				toReturn.add(key);
+		}
+		return toReturn;
+	}
+	
+	/** @return {@code true} if this object contains no properties */
+	public boolean isEmpty() {
+		return size() <= 0;
+	}
+	
+	/** @return number of properties */
+	public int size() {
+		return keys().size();
 	}
 	
 	/**
 	 * Clears all properties from memory.
-	 * Any properties not saved to disk are lost.
 	 */
 	public void clear() {
-		properties.clear();
+		keys.clear();
+		values.clear();
+		keyPositions.clear();
 	}
 	
 	/**
-	 * Loads all properties found in this instance's respective file.
+	 * Resets all properties to default values. If this object does not have specified default values, clears all properties instead.
+	 */
+	public void loadDefaults() {
+		clear();
+		
+		if (defaults != null) {
+			for (String key : defaults.keys())
+				put(key, defaults.get(key));
+		}
+	}
+	
+	/**
+	 * Loads all properties found in this object's backing file. If this object does not have a backing file, this method does nothing.
 	 * @throws IOException if an I/O error occurs
 	 */
-	public void loadFromFile() throws IOException {
+	public void loadFile() throws IOException {
+		if (file == null)
+			return;	// No file, no load
+		
 		FileReader fileIn;
 		try {
-			fileIn = new FileReader(new File(filename));
+			fileIn = new FileReader(file);
 		} catch (FileNotFoundException e) {
-			saveToFile();
-			fileIn = new FileReader(new File(filename));	// Should open now
+			saveFile();	// Create file
+			fileIn = new FileReader(file);	// Open again
 		}
 		try (BufferedReader fileReader = new BufferedReader(fileIn)) {
 		
 			String nextLine;
 			while ((nextLine = fileReader.readLine()) != null) {
-				String[] currentKeyValue = nextLine.split("=");	// Line should be "<KEY>=<VALUE>";
-				String currentKey = "", currentValue = "";
+				String[] currentKeyValue = nextLine.split(DELIMETER);
+				String 	currentKey = null,
+								currentValue = null;
 				
-				if (currentKeyValue.length > 0) {
+				if (currentKeyValue.length > 0 && currentKeyValue[0].length() > 0) {
 					currentKey = currentKeyValue[0].trim();
 					if (currentKeyValue.length > 1) {
 						currentValue = currentKeyValue[1].trim();
 					}
 				}
-				properties.put(currentKey, currentValue);
+				put(currentKey, currentValue);
 			}
 		}
 	}
 	/**
-	 * Writes all properties currently loaded in memory to the properties file.
+	 * Writes all current properties to the backing file.
 	 * @throws IOException if an I/O error occurs
-	 * @throws FileNotFoundException  if the properties file cannot be accessed for some reason
+	 * @throws FileNotFoundException  if the backing file cannot be accessed for some reason
 	 */
-	public void saveToFile() throws FileNotFoundException, IOException {
-		try (OutputStream fileOut = new FileOutputStream(new File(filename));
+	public void saveFile() throws FileNotFoundException, IOException {
+		try (	OutputStream fileOut = new FileOutputStream(file);
 					PrintWriter filePrinter = new PrintWriter(fileOut)) {
-			for (String key : properties.keySet()) {
-				filePrinter.println(key + "=" + properties.get(key));
+			for (String key : keys) {
+				filePrinter.print(key);
+				
+				if (!key.contains(COMMENT))	// If the current key is not a comment
+					filePrinter.print(DELIMETER + get(key));
+				
+				filePrinter.println();
 			}
 		}
 	}
 	
-	/** @return name of file attached to this instance */
-	public String getFilename() {
-		return filename;
+	/** @return backing file */
+	public File getFile() {
+		return file;
+	}
+	/**
+	 * @param newFile new backing file
+	 * @param mkdirs if {@code true}, the path to the specified file is created if it does not exist
+	 */
+	public void setFile(File newFile, boolean mkdirs) {
+		file = newFile;
+		if (mkdirs)
+			tryMkdirs();
+	}
+	private void tryMkdirs() {
+		File parent = file.getParentFile();
+		if (parent != null && !parent.exists()) {
+			parent.mkdirs();
+		}
+	}
+	
+	/** @return default properties */
+	public Properties getDefaults() {
+		return defaults;
+	}
+	/** @param newDefaults new default properties */
+	public void setDefaults(Properties newDefaults) {		
+		defaults = newDefaults;
 	}
 }
--- test/dev/kkorolyov/simpleprops/PropertiesTest.java
+++ test/dev/kkorolyov/simpleprops/PropertiesTest.java
@@ -1,61 +1,45 @@
 package dev.kkorolyov.simpleprops;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 @SuppressWarnings("javadoc")
 public class PropertiesTest {	// TODO Finish
 	private static final int NUM_FILES = 5;
-	private static final String[] ALL_FILENAMES = new String[NUM_FILES];
-	@SuppressWarnings("unchecked")
-	private static final Map<String, String>[] ALL_DEFAULT_PROPERTIES = new HashMap[NUM_FILES];
-	private static final String[][][] ALL_DEFAULT_PROPERTIES_ARRAY = new String[NUM_FILES][][];
+	private static final File[] ALL_FILES = new File[NUM_FILES];
+	private static final Properties[] ALL_DEFAULTS = new Properties[NUM_FILES];
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws IOException {
-		buildFilenames();
-		buildPropertiesCollection();
-		
-		for (int i = 0; i < NUM_FILES; i++) {
-			if (i % 2 == 0)
-				Properties.getInstance(ALL_FILENAMES[i], ALL_DEFAULT_PROPERTIES[i]);
-			else
-				Properties.getInstance(ALL_FILENAMES[i], ALL_DEFAULT_PROPERTIES_ARRAY[i]);
-		}
-	}
-	@Before
-	public void setUpBefore() throws IOException {
-		for (String filename : ALL_FILENAMES)
-			Properties.getInstance(filename).loadDefaults();
+		buildFiles();
+		buildDefaults();
 	}
 	
 	@After
 	public void tearDownAfter() throws IOException {
-		for (String filename : ALL_FILENAMES)
-			new File(filename).delete();
+		for (File file : ALL_FILES)
+			file.delete();
 	}
 	
 	@Test
 	public void testLoadDefaults() throws IOException {
 		for (int i = 0; i < NUM_FILES; i++) {
-			String currentFilename = ALL_FILENAMES[i];
-			Map<String, String> currentDefaults = ALL_DEFAULT_PROPERTIES[i];
-			Properties currentInstance = Properties.getInstance(currentFilename);
+			File currentFile = ALL_FILES[i];
+			Properties currentDefaults = ALL_DEFAULTS[i];
+			Properties currentInstance = new Properties(currentFile, currentDefaults);
 						
 			assertEquals(currentDefaults.size(), currentInstance.size());
 			
-			for (String key : currentDefaults.keySet()) {
-				assertEquals(currentDefaults.get(key), currentInstance.getValue(key));
+			for (String key : currentDefaults.keys()) {
+				assertEquals(currentDefaults.get(key), currentInstance.get(key));
 			}
 		}
 	}
@@ -63,92 +47,74 @@ public class PropertiesTest {	// TODO Finish
 	@Test
 	public void testAddProperty() throws IOException {
 		for (int i = 0; i < NUM_FILES; i++) {
-			String currentFilename = ALL_FILENAMES[i];
-			Map<String, String> currentDefaults = ALL_DEFAULT_PROPERTIES[i];
-			Properties currentInstance = Properties.getInstance(currentFilename);
+			File currentFile = ALL_FILES[i];
+			Properties currentDefaults = ALL_DEFAULTS[i];
+			Properties currentInstance = new Properties(currentFile, currentDefaults);
 			
 			String 	newKey = "NEW-KEY" + i,
 							newValue = "NEW-VAL" + i;
-			currentInstance.addProperty(newKey, newValue);
+			currentInstance.put(newKey, newValue);
 			
 			assertTrue(currentInstance.size() == currentDefaults.size() + 1);
-			assertEquals(newValue, currentInstance.getValue(newKey));
+			assertEquals(newValue, currentInstance.get(newKey));
 		}
 	}
 
 	@Test
 	public void testClear() throws IOException {
 		for (int i = 0; i < NUM_FILES; i++) {
-			String currentFilename = ALL_FILENAMES[i];
-			Map<String, String> currentDefaults = ALL_DEFAULT_PROPERTIES[i];
-			Properties currentInstance = Properties.getInstance(currentFilename, currentDefaults);	// Test newDefaults ignoring
+			File currentFile = ALL_FILES[i];
+			Properties currentDefaults = ALL_DEFAULTS[i];
+			Properties currentInstance = new Properties(currentFile, currentDefaults);
 			
-			currentInstance.addProperty("Extra", "Extra");
-			
-			assertTrue(currentInstance.size() == currentDefaults.size() + 1);
+			currentInstance.put("Extra", "Extra");
+			assertEquals(currentDefaults.size() + 1, currentInstance.size());
 
 			currentInstance.clear();
-			
-			assertEquals(currentDefaults.size(), currentInstance.size());
+			assertEquals(0, currentInstance.size());
 		}
 	}
 
 	@Test
 	public void testSaveToFile() throws IOException {
 		for (int i = 0; i < NUM_FILES; i++) {
-			String currentFilename = ALL_FILENAMES[i];
-			Map<String, String> currentDefaults = ALL_DEFAULT_PROPERTIES[i];
-			Properties currentInstance = Properties.getInstance(currentFilename);	// Test newDefaults ignoring
+			File currentFile = ALL_FILES[i];
+			Properties currentDefaults = ALL_DEFAULTS[i];
+			Properties currentInstance = new Properties(currentFile, currentDefaults);
 			
-			String 	changedKey = currentDefaults.keySet().iterator().next(),	// 1st key
+			String 	changedKey = currentDefaults.keys().get(0),	// 1st key
 							newValue = "NEW-VAL" + i;
-			currentInstance.addProperty(changedKey, newValue);
+			currentInstance.put(changedKey, newValue);
 			
-			currentInstance.saveToFile();
+			currentInstance.saveFile();
 
 			assertEquals(currentDefaults.size(), currentInstance.size());
-			assertTrue(!currentInstance.getValue(changedKey).equals(currentDefaults.get(changedKey)));
+			assertFalse(currentInstance.get(changedKey).equals(currentDefaults.get(changedKey)));
 			
 			currentInstance.loadDefaults();
 			
 			assertEquals(currentDefaults.size(), currentInstance.size());
-			assertEquals(currentDefaults.get(changedKey), currentInstance.getValue(changedKey));
+			assertEquals(currentDefaults.get(changedKey), currentInstance.get(changedKey));
 			
-			currentInstance.loadFromFile();
+			currentInstance.loadFile();
 			
 			assertEquals(currentDefaults.size(), currentInstance.size());
-			assertTrue(!currentInstance.getValue(changedKey).equals(currentDefaults.get(changedKey)));
+			assertFalse(currentInstance.get(changedKey).equals(currentDefaults.get(changedKey)));
 		}
 	}
 	
-	private static void buildFilenames() {		
+	private static void buildFiles() {		
 		for (int i = 0; i < NUM_FILES; i++) {
-			ALL_FILENAMES[i] = "TestPropFile" + i + ".txt";
+			ALL_FILES[i] = new File("TestPropFile" + i + ".txt");
 		}
 	}
-	private static void buildPropertiesCollection() {
-		for (int i = 0; i < ALL_DEFAULT_PROPERTIES.length; i++) {
-			ALL_DEFAULT_PROPERTIES[i] = buildPropertiesMap(i + 1);
-			
-			ALL_DEFAULT_PROPERTIES_ARRAY[i] = buildProperties(i + 1);
+	private static void buildDefaults() {
+		for (int i = 0; i < ALL_DEFAULTS.length; i++) {
+			Properties currentDefaults = new Properties();
+			for (int j = 0; j < i + 1; j++) {
+				currentDefaults.put("KEY" + j, "VAL" + j);
+			}
+			ALL_DEFAULTS[i] = currentDefaults;
 		}
-	}
-
-	private static String[][] buildProperties(int numProperties) {
-		String[][] properties = new String[numProperties][2];
-		
-		for (int i = 0; i < properties.length; i++) {
-			properties[i][0] = "KEY" + i;
-			properties[i][1] = "VAL" + i;
-		}
-		return properties;
-	}
-	private static Map<String, String> buildPropertiesMap(int numProperties) {
-		Map<String, String> properties = new HashMap<>();
-		
-		for (int i = 0; i < numProperties; i++) {
-			properties.put("KEY" + i, "VAL" + i);
-		}
-		return properties;
 	}
 }
