2feaf38639ce27acbdea02d2e0b558d6c6b5ce62
kkorolyov <kirillk753@gmail.com>
2018-04-01 14:23:05 -0700

    More throwing functions: Add ThrowingPredicates

---
+++ simple-funcs/src/test/groovy/dev/kkorolyov/simplefuncs/function/ThrowingBiPredicateSpec.groovy
@@ -0,0 +1,20 @@
+package dev.kkorolyov.simplefuncs.function
+
+import spock.lang.Specification
+
+class ThrowingBiPredicateSpec extends Specification {
+	ThrowingBiPredicate<?, ?, Exception> throwingBiPredicate = new ThrowingBiPredicate<Object, Object, Exception>() {
+		@Override
+		boolean testThrowing(Object o, Object o2) throws Exception {
+			throw new Exception()
+		}
+	}
+
+	def "wraps as runtime exception"() {
+		when:
+		throwingBiPredicate.test(new Object(), new Object())
+
+		then:
+		thrown RuntimeException
+	}
+}
